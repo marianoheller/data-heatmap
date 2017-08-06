1a1f628d54aadfcaaa5d7bcb2da4da4eb410d974
Mariano Mariano <Mariano Mariano>
2017-08-05 21:12:18 -0300

    Sabado tarde

--- src/Heatmap/Heatmap.js
+++ src/Heatmap/Heatmap.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
+import d3tip from "d3-tip";
 
 import './Heatmap.css';
 
@@ -30,7 +31,7 @@ export default class HeatmapContainer extends Component {
     }
 
     loadData() {
-        const { baseTemperature, monthlyVariance } = dataJson;
+        const { monthlyVariance } = dataJson;
         
         return monthlyVariance;
     }
@@ -67,18 +68,25 @@ class Heatmap extends Component {
 
     
     createHeatmap() {
-        const node = this.node;
         const { dataset, baseValue } = this.props;
 
         const opts = {
             width: 1000,
-            height: 600,
+            height: 500,
             margin: {
                 top: 50,
                 right: 0,
-                bottom: 100,
-                left: 30
-            }
+                bottom: 50,
+                left: 50
+            },
+            colors: {
+                domain: [
+                            baseValue + d3.min(dataset  , (d) => d.variance) , 
+                            baseValue,
+                            baseValue + d3.max(dataset  , (d) => d.variance )
+                ],
+                range: ["#0000CC", "#f7ff89", "#ba0000"],
+            },
         }
         
         const svg = d3.select("#heatmapContainer")
@@ -88,68 +96,107 @@ class Heatmap extends Component {
                     .append("g")
                     .attr("transform", `translate(${opts.margin.left},${opts.margin.top})`);
 
-        const xScale =  d3.scaleLinear()
-                        .domain([d3.min(dataset  , (d) => d.year) , d3.max(dataset  , (d) => d.year )])
-                        .range([0,opts.width - opts.margin.left - opts.margin.right])
+        const heightSquare = ( (opts.height - opts.margin.top - opts.margin.bottom) / 12 );
+        const widthSquare = ( (opts.width - opts.margin.left - opts.margin.right) / (dataset.length / 12) );
+
+        const xScale =  d3.scaleTime()
+                        .domain([
+                            new Date(d3.min(dataset  , (d) => d.year), 0 ) , 
+                            new Date(d3.max(dataset  , (d) => d.year)+1, 11 )
+                        ])
+                        .range([0, (opts.width - opts.margin.left - opts.margin.right)-widthSquare]);
 
         const yScale =  d3.scaleLinear()
                         .domain([d3.min(dataset  , (d) => d.month) , d3.max(dataset  , (d) => d.month )])
-                        .range([0,opts.height - opts.margin.top - opts.margin.bottom])
-
-        const widthScale =  d3.scaleLinear()
-                            .domain([d3.min(dataset  , (d) => d.month) , d3.max(dataset  , (d) => d.month )])
-                            .range([0,opts.height - opts.margin.top - opts.margin.bottom])
+                        .range([0,(opts.height - opts.margin.top - opts.margin.bottom)- heightSquare])
 
         const colorScale =  d3.scaleLinear()
-                            .domain([d3.min(dataset  , (d) => d.variance) , d3.max(dataset  , (d) => d.variance )])
-                            .rangeRound([0,255]);
+                            .domain( opts.colors.domain )
+                            .range( opts.colors.range );
 
-        const xAxis = d3.axisBottom(xScale);
+        const xAxis =   d3.axisBottom(xScale).tickArguments([d3.timeYear.every(10)]);
 
         const yAxis = d3.axisLeft(yScale);
 
-        /* const tip =  d3.tip()
+        const tip =  d3tip()
                     .attr('class', 'd3-tip')
                     .html( (d) =>  {
-                        return `<strong>Variance:</strong> <span style='color:red'>${baseValue - d.variance}</span>`;
-                    }); */
+                        return `
+                        <strong>Year:</strong> ${d.year}<br>
+                        <strong>Month:</strong> ${d.month}<br>
+                        <strong>Temperature:</strong> ${Math.round( (baseValue + d.variance) * 100) / 100}
+                         
+                        `;
+                    }); 
+        svg.call(tip);
 
-        /* const tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d; }); */
-
+        svg.append("text")
+        .attr("x", (opts.width / 2))             
+        .attr("y", 0 - (opts.margin.top / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "16px") 
+        .style("text-decoration", "underline")  
+        .text("Monthly Global Land-Surface Temperature");
 
         svg.selectAll("rect")
         .data(dataset)
         .enter()
         .append("rect")
         .attr("x", (d) => {
-            return xScale(d.year);
+            return xScale( new Date(d.year, d.month-1));
         })
         .attr("y", (d) => {
             return yScale(d.month);
         })
         .attr("height", (d) => {
-            return ( (opts.height - opts.margin.top - opts.margin.bottom) / 12 );
+            return heightSquare;
         })
         .attr("width", (d) => {
-            return ( (opts.width - opts.margin.left - opts.margin.right) / (dataset.length / 12) );
+            return widthSquare;
         })
-        .attr("stroke", (d) => {
-            return `rgba(${colorScale(d.variance)},75,${255 - colorScale(d.variance)}, 1)`;
-        })
+        .attr("stroke", (d) => colorScale( baseValue + d.variance ))
         .attr("stroke-width", 1)
-        .attr("fill", (d) => {
-            return `rgba(${colorScale(d.variance)},100,${255 - colorScale(d.variance)}, 0.75)`;
-        })
-        /* .on('mouseover', tip.show)
-        .on('mouseout', tip.hide) */
+        .attr("fill", (d) => colorScale( baseValue + d.variance ))
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 
         svg.append("g")
-        .attr("transform", `translate(0, ${opts.height - opts.margin.top})`)
+        .attr("transform", `translate(0, ${opts.height - opts.margin.top - opts.margin.bottom})`)
         .call(xAxis);
 
         svg.append("g")
-        .attr("transform", `translate(0 , 0)`)
-        .call(yAxis);
+        .selectAll("text")
+        .data(months)
+        .enter()
+        .append("text")
+        .style("text-anchor", "end")
+        .attr("class", "axis-label")
+        .attr("x", 0)
+        .attr("y", (d, i) => {
+            const heightSquare = ( (opts.height - opts.margin.top - opts.margin.bottom ) / 12 );
+            return heightSquare*(i) + heightSquare/2;   
+        })
+        .attr("transform", `translate(${-4}, 0)`)
+        .text( (m) => {
+            return m.split("").filter( (e,i) => i < 3).join("") + ".";
+        });
+
+        svg.append("g")
+        .selectAll("rect")
+        .data(opts.colors.range)
+        .enter()
+        .append("rect")
+        .attr("fill", (d) => d)
+        .attr("x", (d,i) => {
+            return opts.width - opts.margin.left -  (i+1)*40;
+        } )
+        .attr("y", (d,i) => {
+            return opts.height - 20;
+        })
+        .attr("width", 50)
+        .attr("height", 50)
+
+        
     }
     
     render() {
